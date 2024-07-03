a46fe51f76a6d179e30c403b3c70c1063b24f776
Ser-D <ipc.avanti@gmail.com>
2024-07-03 14:56:31 +0300

    Remove .idea, configure pre-commit, and fix code issues

--- app/main.py
+++ app/main.py
@@ -6,13 +6,9 @@ app = FastAPI()
 
 @app.get("/")
 def health_check():
-    return {
-        "status_code": 200,
-        "detail": "ok",
-        "result": "working"
-    }
+    return {"status_code": 200, "detail": "ok", "result": "working"}
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
     # uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
-    #uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
