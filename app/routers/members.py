5b5afa34230c4e860d47bd0e4158fd7654da20c3
Ser-D <ipc.avanti@gmail.com>
2024-07-26 10:53:15 +0300

    Update

--- app/routers/members.py
+++ app/routers/members.py
@@ -37,36 +37,3 @@ async def accept_request(
         db, user_id, company_id
     )
     return accepted_member
-
-
-@router.post("/join", response_model=MemberDetail)
-async def join_company(
-    company_id: int,
-    db: AsyncSession = Depends(get_database),
-    current_user: User = Depends(auth_service.get_current_user),
-):
-    await member_repository.company_exists(db, company_id)
-    await member_repository.member_exists(db, current_user.id, company_id)
-
-    member_create = MemberCreate(
-        company_id=company_id,
-        user_id=current_user.id,
-        is_admin=False,
-        status="pending",
-        type="request",
-    )
-    return await member_repository.create_member(db, member_create)
-
-
-@router.post("/leave", response_model=MemberDetail)
-async def leave_company(
-    company_id: int,
-    db: AsyncSession = Depends(get_database),
-    current_user: User = Depends(auth_service.get_current_user),
-):
-    await member_repository.get_member(db, current_user.id, company_id)
-
-    deleted_member = await member_repository.delete_member(
-        db, current_user.id, company_id
-    )
-    return deleted_member
