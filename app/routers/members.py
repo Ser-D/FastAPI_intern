94f19415f21e22b26c8cce43a8c8fc1d8772d1d3
Ser-D <ipc.avanti@gmail.com>
2024-07-26 10:55:57 +0300

    Update

--- app/routers/members.py
+++ app/routers/members.py
@@ -12,11 +12,11 @@ router = APIRouter(prefix="/companies/{company_id}/members", tags=["members"])
 
 @router.get("/view", response_model=list[MemberDetail])
 async def membership_company(
-    company_id: int,
-    db: AsyncSession = Depends(get_database),
-    current_user: User = Depends(auth_service.get_current_user),
-    type: str = Query(None, description="Type of membership: invite or request"),
-    status: str = Query(None, description="Status of membership: active or pending"),
+        company_id: int,
+        db: AsyncSession = Depends(get_database),
+        current_user: User = Depends(auth_service.get_current_user),
+        type: str = Query(None, description="Type of membership: invite or request"),
+        status: str = Query(None, description="Status of membership: active or pending"),
 ):
     await member_repository.is_owner(db, current_user.id, company_id)
     memberships = await member_repository.get_memberships_my_company(
@@ -27,10 +27,10 @@ async def membership_company(
 
 @router.post("/accept_request", response_model=MemberDetail)
 async def accept_request(
-    user_id: int,
-    company_id: int,
-    db: AsyncSession = Depends(get_database),
-    current_user: User = Depends(auth_service.get_current_user),
+        user_id: int,
+        company_id: int,
+        db: AsyncSession = Depends(get_database),
+        current_user: User = Depends(auth_service.get_current_user),
 ):
     await member_repository.is_owner(db, current_user.id, company_id)
     accepted_member = await member_repository.accept_membership_request(
@@ -39,44 +39,11 @@ async def accept_request(
     return accepted_member
 
 
-@router.post("/join", response_model=MemberDetail)
-async def join_company(
-    company_id: int,
-    db: AsyncSession = Depends(get_database),
-    current_user: User = Depends(auth_service.get_current_user),
-):
-    await member_repository.company_exists(db, company_id)
-    await member_repository.member_exists(db, current_user.id, company_id)
-
-    member_create = MemberCreate(
-        company_id=company_id,
-        user_id=current_user.id,
-        is_admin=False,
-        status="pending",
-        type="request",
-    )
-    return await member_repository.create_member(db, member_create)
-
-
-@router.post("/leave", response_model=MemberDetail)
-async def leave_company(
-    company_id: int,
-    db: AsyncSession = Depends(get_database),
-    current_user: User = Depends(auth_service.get_current_user),
-):
-    await member_repository.get_member(db, current_user.id, company_id)
-
-    deleted_member = await member_repository.delete_member(
-        db, current_user.id, company_id
-    )
-    return deleted_member
-
-
 @router.get("/admins", response_model=list[MemberDetail])
 async def get_admins(
-    company_id: int,
-    db: AsyncSession = Depends(get_database),
-    current_user: User = Depends(auth_service.get_current_user),
+        company_id: int,
+        db: AsyncSession = Depends(get_database),
+        current_user: User = Depends(auth_service.get_current_user),
 ):
     await member_repository.is_owner(db, current_user.id, company_id)
     admins = await member_repository.get_admins(db, company_id)
@@ -85,10 +52,10 @@ async def get_admins(
 
 @router.post("/assign_admin", response_model=MemberDetail)
 async def assign_admin(
-    company_id: int,
-    user_id: int,
-    db: AsyncSession = Depends(get_database),
-    current_user: User = Depends(auth_service.get_current_user),
+        company_id: int,
+        user_id: int,
+        db: AsyncSession = Depends(get_database),
+        current_user: User = Depends(auth_service.get_current_user),
 ):
     await member_repository.is_owner(db, current_user.id, company_id)
     admin_member = await member_repository.assign_admin(db, user_id, company_id)
@@ -97,10 +64,10 @@ async def assign_admin(
 
 @router.post("/remove_admin", response_model=MemberDetail)
 async def remove_admin(
-    company_id: int,
-    user_id: int,
-    db: AsyncSession = Depends(get_database),
-    current_user: User = Depends(auth_service.get_current_user),
+        company_id: int,
+        user_id: int,
+        db: AsyncSession = Depends(get_database),
+        current_user: User = Depends(auth_service.get_current_user),
 ):
     await member_repository.is_owner(db, current_user.id, company_id)
     non_admin_member = await member_repository.remove_admin(db, user_id, company_id)
